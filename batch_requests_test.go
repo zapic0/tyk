3bcc9a07bc2bdb69c531d1d663a99d45df3f74a4
joshblakeley <jtblakeley@gmail.com>
2018-02-13 11:06:46 +0000

    batch requests client checks for ssl config and add test

--- batch_requests_test.go
+++ batch_requests_test.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"crypto/tls"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/TykTechnologies/tyk/apidef"
+	"github.com/TykTechnologies/tyk/config"
 	"github.com/TykTechnologies/tyk/test"
 )
 
@@ -64,4 +72,82 @@ func TestBatch(t *testing.T) {
 			t.Error("Url order not match:", string(batchResponse[0]["relative_url"]))
 		}
 	}
+}
+
+var virtBatchTest = `function batchTest (request, session, config) {
+    // Set up a response object
+    var response = {
+        Body: ""
+        Headers: {
+            "content-type": "application/json"
+        },
+        Code: 202
+    }
+    
+    // Batch request
+    var batch = {
+        "requests": [
+			{
+				"method": "GET",
+				"relative_url": "{upstream_URL}"
+			},
+		],
+        "suppress_parallel_execution": false
+    }
+
+    var newBody = TykBatchRequest(JSON.stringify(batch))
+    
+    
+    var asJS = JSON.parse(newBody)
+    for (var i in asJS) {
+        asJS[i].body = JSON.parse(asJS[i].body)
+    }
+    
+    // We need to send a string object back to Tyk to embed in the response
+    response.Body = JSON.stringify(asJS)
+    
+    return TykJsResponse(response, session.meta_data)
+    
+}`
+
+func TestSSLBatch(t *testing.T) {
+
+	upstream := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	}))
+
+	upstream.TLS = &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	upstream.StartTLS()
+	virtBatchTest = strings.Replace(virtBatchTest, "{upstream_URL}", upstream.URL, 1)
+	fmt.Println(virtBatchTest)
+	defer upstream.Close()
+
+	config.Global.ProxySSLInsecureSkipVerify = true
+
+	defer resetTestConfig()
+
+	ts := newTykTestServer()
+	defer ts.Close()
+
+	buildAndLoadAPI(func(spec *APISpec) {
+		spec.Proxy.ListenPath = "/"
+		virtualMeta := apidef.VirtualMeta{
+			ResponseFunctionName: "batchTest",
+			FunctionSourceType:   "blob",
+			FunctionSourceURI:    base64.StdEncoding.EncodeToString([]byte(virtBatchTest)),
+			Path:                 "/virt",
+			Method:               "GET",
+		}
+		v := spec.VersionData.Versions["v1"]
+		v.UseExtendedPaths = true
+		v.ExtendedPaths = apidef.ExtendedPathsSet{
+			Virtual: []apidef.VirtualMeta{virtualMeta},
+		}
+		spec.VersionData.Versions["v1"] = v
+	})
+
+	ts.Run(t, test.TestCase{
+		Path: "/virt", Code: 202,
+	})
 }
